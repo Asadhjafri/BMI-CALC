e9564d01f47cb4ad86752d0634a8487c6f89a237
Asad Jafri <51427124+Asadhjafri@users.noreply.github.com>
2022-11-08 19:36:29 +0000

    check bmi for 18.5 and 25 and print appropriate responses.

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+
+bmi = round(weight / height ** 2)
+
+if bmi < 18.5:
+  print(f"Your bmi is {bmi}, you are underweight.")
+elif bmi < 25:
+  print (f"Your bmi is {bmi}, you have a normal weight")
